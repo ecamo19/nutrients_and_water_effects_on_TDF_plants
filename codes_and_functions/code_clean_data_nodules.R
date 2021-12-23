1c800cce0a0aa4116493293cc99ed8cce51e0547
ecamo19 <erickcamo19@gmail.com>
2021-12-23 15:16:01 -0500

    code data nodules  updated

--- codes_and_functions/code_clean_data_nodules.R
+++ codes_and_functions/code_clean_data_nodules.R
@@ -1,79 +1,67 @@
-# Load packages -----------------------------------------------------------
-library(tidyverse)
+# Load packages ----------------------------------------------------------------
+library(dplyr)
 library(janitor)
 
-
-# Load nodule and height data ----------------------------------------------
+# Load nodule and height data --------------------------------------------------
 
 data_nodules <- 
-	read.csv("~/Documents/projects/shade_house_experiment/data/raw_data/1_nodule_data.csv", header = T) %>%
+	read.csv("./raw_data/1_nodule_data.csv", header = T) %>%
 	clean_names()
 
 data_init_height <- 
-	read.csv("~/Documents/projects/shade_house_experiment/data/raw_data/data_heights.csv", header = T) %>%
+	read.csv("./raw_data/data_heights.csv", header = T) %>%
 	clean_names() %>% 
 	
-	#remove unused columns
+    #remove unused columns
 	dplyr::select(1:5)
 
 
-# Transform to factor spcode,id and treatment -----------------------------------
+# Clean nodules datase ---------------------------------------------------------
 
-data_nodules <- 
+data_nodules_cleaned  <- 
 	data_nodules %>% 
+    
+    # Transform to factor spcode,id and treatment 
 	mutate(id = factor(id),
 		   spcode = factor(spcode),
-		   treatment = factor(treatment))
-	
+		   treatment = factor(treatment,
+		                      levels = c("ambientrain", 
+		                                "ambientrain_nutrients",
+		                                "ambientrain_water",
+		                                "ambientrain_water_nutrients"))) %>%
+    # Order columns
+    dplyr::select(id, spcode, treatment, everything())
 
 
-#Order factors 
-data_nodules$treatment <-
-	factor(data_nodules$treatment,
-		   levels = c(
-		   	"ambientrain", 
-		   	"ambientrain_nutrients",
-		   	"ambientrain_water",
-		   	"ambientrain_water_nutrients"
-		   )
-	)
 
-
-# Clean data and join initial height --------------------------------------------
-
-#Select nfixer species
+# Clean data initial height ----------------------------------------------------
 
 data_init_height_nfixer <- 
-	data_init_height  %>% 
+	data_init_height  %>%
+    
+    # Select nfixer species
 	filter(family == "Legume" & spcode != "hc") %>% 
-	
-	#Remove unused columns 
-	dplyr::select(-c(family)) %>% 
-	
-	#Transform columns to a factor
-	mutate(spcode = factor(spcode),
-		   treatment = factor(treatment),
-		   id = factor(id)) %>% 
-		
-	rename(init_height = "x20150831")
-	 
-	
-data_nodules_cleaned <-
-	data_nodules %>%
-	
-	dplyr::select(id,spcode,treatment, everything())
-	
+    
+    #Remove unused columns 
+    dplyr::select(-c(family)) %>% 
+    
+    #Transform columns to a factor
+    mutate(spcode = factor(spcode),
+           treatment = factor(treatment),
+           id = factor(id)) %>% 
+    
+    rename(init_height = "x20150831")
 
-#Join data sets
-#left_join(x, y, by = NULL, copy = FALSE, suffix = c(".x", ".y"), ...)
+
+# Join data sets ---------------------------------------------------------------
 
 data_nodules_cleaned <- 
-	left_join(data_nodules_cleaned,data_init_height_nfixer , 
-			  by = c('id','treatment','spcode')) 
+	inner_join(data_nodules_cleaned, data_init_height_nfixer ,
+	           by = c('id','treatment','spcode')) 
 
 
-
-
-# Remove unused data sets -------------------------------------------------------
+# Remove unused data sets ------------------------------------------------------
 rm(data_nodules)
 rm(data_init_height)
+rm(data_init_height_nfixer)
+
