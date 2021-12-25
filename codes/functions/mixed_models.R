7637ae8edae62aedc077c72605ed6588ed1019e0
ecamo19 <erickcamo19@gmail.com>
2021-12-25 18:45:35 -0500

    Models with random slopes and intercepts not implemented

--- codes/functions/mixed_models.R
+++ codes/functions/mixed_models.R
@@ -37,33 +37,31 @@ model_1_1_varident <- function(response, data){
 
 ## Model 2: random effect treatment|spcode -------------------------------------
 
-mixed_model_2_treatment_spcode_random <-  function(response, data) {
-    
-    #this function takes each response variable and join it to the formula
-    formula <-  paste(response, " ~ nfixer*treatment + init_height")
-    
-    # Fit the model
-    lme(as.formula(formula), 
-        random = ~ treatment|spcode, 
-        method = "REML",
-        data = data)
-}
-
+# mixed_model_2_treatment_spcode_random_lmer <-  function(response, data) {
+#     
+#     #this function takes each response variable and join it to the formula
+#     formula <-  
+#         paste(response, " ~ nfixer*treatment + init_height + (1 + spcode |treatment)")
+#     
+#     # Fit the model
+#     lmer(as.formula(formula), data = data)
+# }
+# 
 
 ## Model 2.1 varIdent ----------------------------------------------------------
 
-model_2_1_varident <- function(response, data){
-    
-    #this function takes each response variable and join it to the formula
-    formula <-  paste(response, " ~ nfixer*treatment + init_height")
-    
-    # Fit the model
-    lme(as.formula(formula),
-        random = ~ treatment|spcode, 
-        method = "REML",
-        weights = varIdent(form = ~ 1 | treatment*nfixer),
-        data = data)
-}
-
+# model_2_1_varident <- function(response, data){
+#     
+#     #this function takes each response variable and join it to the formula
+#     formula <-  paste(response, " ~ nfixer*treatment + init_height")
+#     
+#     # Fit the model
+#     lme(as.formula(formula),
+#         random = ~ treatment|spcode, 
+#         method = "REML",
+#         weights = varIdent(form = ~ 1 | treatment*nfixer),
+#         data = data)
+# }
+# 
 
 
