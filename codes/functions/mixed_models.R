667906478aedbbce5a15f6238a2ebb3649d82f59
ecamo19 <erickcamo19@gmail.com>
2021-12-25 18:02:11 -0500

    mixed model functions created

---
+++ codes/functions/mixed_models.R
@@ -0,0 +1,69 @@
+# Objetive ---------------------------------------------------------------------
+# This script contains all the models used to analyze the data from the shade 
+# house experiment 
+
+# Load packages ----------------------------------------------------------------
+library(lme4)
+
+
+# Models -----------------------------------------------------------------------
+
+
+## Model 1: random effect 1|spcode ---------------------------------------------
+
+mixed_model_1_spcode_random <-  function(response, data) {
+    
+    #this function takes each response variable and join it to the formula
+    formula <-  paste(response, " ~ nfixer*treatment + init_height")
+    
+    # Fit the model
+    lme(as.formula(formula),random = ~ 1|spcode, method = "REML",
+        data = data)
+}
+
+
+## Model 1.1 VarIdent model -----------------------------------------------------
+
+model_1_1_varident <- function(response, data){
+            
+        #this function takes each response variable and join it to the formula
+        formula <-  paste(response, " ~ nfixer*treatment + init_height")
+        
+        # Fit the model
+        lme(as.formula(formula),random = ~ 1|spcode, method = "REML",
+            weights = varIdent(form = ~ 1 | treatment*nfixer),
+            data = data)
+}
+
+## Model 2: random effect treatment|spcode -------------------------------------
+
+mixed_model_2_treatment_spcode_random <-  function(response, data) {
+    
+    #this function takes each response variable and join it to the formula
+    formula <-  paste(response, " ~ nfixer*treatment + init_height")
+    
+    # Fit the model
+    lme(as.formula(formula), 
+        random = ~ treatment|spcode, 
+        method = "REML",
+        data = data)
+}
+
+
+## Model 2.1 varIdent ----------------------------------------------------------
+
+model_2_1_varident <- function(response, data){
+    
+    #this function takes each response variable and join it to the formula
+    formula <-  paste(response, " ~ nfixer*treatment + init_height")
+    
+    # Fit the model
+    lme(as.formula(formula),
+        random = ~ treatment|spcode, 
+        method = "REML",
+        weights = varIdent(form = ~ 1 | treatment*nfixer),
+        data = data)
+}
+
+
+
