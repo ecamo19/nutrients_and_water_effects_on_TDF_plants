00196677c34b23c9bce491afb6f3a510bed6d3cd
ecamo19 <erickcamo19@gmail.com>
2022-01-08 16:36:54 -0500

    lme model was created for using it as base model and comparing it to the varIdent models

--- codes/functions/functions_mixed_models.R
+++ codes/functions/functions_mixed_models.R
@@ -13,25 +13,47 @@ library(lme4)
 # These functions were created for fitting a mixed model with the fixed effects, 
 # treatment, nfixer, their interaction and the covariate initial height 
 # (which takes into account that each seedling had a different height at the 
-# beginning of the experiment). These functions were created for using tehm in 
-# combination with purrr::map to save time. 
+# beginning of the experiment). 
+
+# These functions were created for using them in combination with purrr::map 
+# to save time. 
 
 # Model fitting ----------------------------------------------------------------
 
 ## Model 1: Base model, random effect (1|spcode) -------------------------------
 
+# This is the base model. I created this models using lme4 mainly because the 
+# performance package don't work that good with nlme objects. If I find any
+# assumption violation I used the varIdent models if not I keep the lme4 model
+
 mixed_model_1 <-  function(response, data) {    
     
     # Take each response variable and join it to the model formula
     formula = paste(response, " ~ treatment * nfixer + init_height + (1|spcode)")
-    #fixed_effects <-  paste0(response, " ~ nfixer*treatment + init_height")
     
     # Fit the model
     model_1_base <- lmer(as.formula(formula),
-                            #random = ~ 1|spcode,
-                            #method = "REML",
                             data = data) 
     return(model_1_base)
+    
+}
+
+
+# This model was created for comparing the varIdent models with the base models, 
+# this beacuse a lme4 object cant be used  
+
+mixed_model_1_lme <-  function(response, data) {    
+    
+    # Take each response variable and join it to the model formula
+    fixed_effects <-  paste0(response, " ~ nfixer*treatment + init_height")
+    
+    # Fit the model
+    model_1_base_lme <- lme(as.formula(fixed_effects),
+                         random = ~ 1|spcode,
+                         method = "REML",
+                         data = data) 
+    
+    return(model_1_base_lme)
     
 }
 
@@ -102,7 +124,7 @@ mixed_model_2_varident <- function(response, data,
 }
 
 
-# Model Validation plot varIdent model -----------------------------------------
+# Model Validation plots varIdent models ---------------------------------------
 
 # The coplot generated here if for checking variance hetereogeneity
 
